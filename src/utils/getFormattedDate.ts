45a91f04dfbc7c53a1352e2f446a3f9b55d42c7e
Dmitry Rudenko <d.rudenko@tg.dunice.net>
2023-08-23 12:30:25 +0300

    constants added in utils/getFormattedDate

--- src/utils/getFormattedDate.ts
+++ src/utils/getFormattedDate.ts
@@ -1,1 +1,3 @@
-export const getFormattedDate = (date: Date): string => String(date).slice(0, 10);
+const DATE_START = 0;
+const DATE_END = 10;
+export const getFormattedDate = (date: Date): string => String(date).slice(DATE_START, DATE_END);
